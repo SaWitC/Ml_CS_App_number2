8fcdd34baf4cbf3733b29c302b341a3859b5aff2
unknown <SaWitC>
2022-07-25 22:25:11 +0300

    created class: mlstps; writed coments

---
+++ Ml_Cs_MultiClass_classification/Ml_Cs_MultiClass_classification/ML/Ml_Steps.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.ML;
+using Ml_Cs_MultiClass_classification.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ml_Cs_MultiClass_classification.ML
+{
+    public class Ml_Steps
+    {
+        public static string _appPath = Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
+        public static string _trainDataPath = Path.Combine(_appPath, "..", "..", "..", "Data", "issues_train.tsv");
+        public static string _testDataPath = Path.Combine(_appPath, "..", "..", "..", "Data", "issues_test.tsv");
+        public static string _modelPath = Path.Combine(_appPath, "..", "..", "..", "Models", "model.zip");
+
+        public static MLContext _mlContext = new MLContext(seed: 0);
+        public static PredictionEngine<IssueModel, IssuePrediction> _predEngine;
+        public static ITransformer _trainedModel;
+        public static IDataView _trainingDataView;
+        public static IEstimator<ITransformer> ProccesData()
+        {
+            var pipeline = _mlContext.Transforms.Conversion.MapValueToKey(inputColumnName: "Area", outputColumnName: "Label")
+                .Append(_mlContext.Transforms.Text.FeaturizeText("TitleFeaturized", "Title"))
+                .Append(_mlContext.Transforms.Text.FeaturizeText(inputColumnName: "Description", outputColumnName: "DescriptionFeaturized"))
+                .Append(_mlContext.Transforms.Concatenate("Features", "TitleFeaturized", "DescriptionFeaturized"))
+                .AppendCacheCheckpoint(_mlContext);//for cache using (only for small or midlle datasets)
+
+            return pipeline;
+        }
+
+        public static IEstimator<ITransformer> BuildAndTrainModel(IDataView TrainDataset, IEstimator<ITransformer> pipeline)
+        {
+            Console.WriteLine("Build and Training");
+            var trainingPipeline = pipeline.Append(_mlContext.MulticlassClassification.Trainers.SdcaMaximumEntropy("Label", "Features"))
+                .Append(_mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+
+            _trainedModel = trainingPipeline.Fit(TrainDataset);
+            _predEngine = _mlContext.Model.CreatePredictionEngine<IssueModel, IssuePrediction>(_trainedModel);
+
+            IssueModel issue = new IssueModel()
+            {
+                Title = "WebSockets communication is slow in my machine",
+                Description = "The WebSockets communication used under the covers by SignalR looks like is going slow in my development machine.."
+            };
+
+            var prediction = _predEngine.Predict(issue);
+
+            Console.WriteLine($"=============== Single Prediction just-trained-model - Result: {prediction.Area} ===============");
+            return trainingPipeline;
+        }
+
+        public static void Evaluate(DataViewSchema trainingDataViewSchema)
+        {
+            var testDataView = _mlContext.Data.LoadFromTextFile<IssueModel>(_testDataPath, hasHeader: true);
+
+            var testMetrics = _mlContext.MulticlassClassification.Evaluate(_trainedModel.Transform(testDataView));
+
+
+            Console.WriteLine($"*************************************************************************************************************");
+            Console.WriteLine($"*       Metrics for Multi-class Classification model - Test Data     ");
+            Console.WriteLine($"*------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine($"*       MicroAccuracy:    {testMetrics.MicroAccuracy:0.###}");
+            Console.WriteLine($"*       MacroAccuracy:    {testMetrics.MacroAccuracy:0.###}");
+            Console.WriteLine($"*       LogLoss:          {testMetrics.LogLoss:#.###}");
+            Console.WriteLine($"*       LogLossReduction: {testMetrics.LogLossReduction:#.###}");
+            Console.WriteLine($"*************************************************************************************************************");
+        }
+
+        public static void SaveModelAsFile(MLContext mLContext, DataViewSchema trainingDataSchema, ITransformer model)
+        {
+            mLContext.Model.Save(model, trainingDataSchema, _modelPath);
+        }
+
+        public static void PredictIssue()
+        {
+            ITransformer loadedModel = _mlContext.Model.Load(_modelPath, out var modelInputSchema);
+
+            IssueModel singleIssue = new IssueModel() { Title = "Entity Framework crashes", Description = "When connecting to the database, EF is crashing" };
+
+            _predEngine = _mlContext.Model.CreatePredictionEngine<IssueModel, IssuePrediction>(loadedModel);
+
+            var prediction = _predEngine.Predict(singleIssue);
+
+            Console.WriteLine($"=============== Single Prediction - Result: {prediction.Area} ===============");
+        }
+
+        public static void Ml_Testing(IssueModel testmodel)
+        {
+            ITransformer loadedModel = _mlContext.Model.Load(_modelPath, out var modelInputSchema);
+            _predEngine = _mlContext.Model.CreatePredictionEngine<IssueModel, IssuePrediction>(loadedModel);
+            var prediction = _predEngine.Predict(testmodel);
+            Console.WriteLine("predict ~=   " +prediction.Area);
+        }
+    }
+}
--- Ml_Cs_MultiClass_classification/Ml_Cs_MultiClass_classification/Program.cs
+++ Ml_Cs_MultiClass_classification/Ml_Cs_MultiClass_classification/Program.cs
@@ -7,82 +7,44 @@ namespace Ml_Cs_MultiClass_classification
 {
     class Program
     {
-        private static string _appPath = Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
-        private static string _trainDataPath = Path.Combine(_appPath, "..", "..", "..", "Data", "issues_train.tsv");
-        private static string _testDataPath = Path.Combine(_appPath, "..", "..", "..", "Data", "issues_test.tsv");
-        private static string _modelPath = Path.Combine(_appPath, "..", "..", "..", "Models", "model.zip");
-
-        private static MLContext _mlContext= new MLContext(seed: 0);
-        private static PredictionEngine<IssueModel, IssuePrediction> _predEngine;
-        private static ITransformer _trainedModel;
-        private static IDataView _trainingDataView;
+      
         static void Main(string[] args)
         {
-           // MLContext mLContext = new MLContext(seed: 0);
-
-            _trainingDataView = _mlContext.Data.LoadFromTextFile<IssueModel>(_trainDataPath, hasHeader: true);
-
-            var pipeline = ProccesData();
-
-            var trainingPipelane = BuildAndTrainModel(_trainingDataView, pipeline);
-
-            Evaluate(_trainingDataView.Schema);
-
-           // SaveModelAsFile(,_trainingDataView,_modelPath);
-        }
-
-        public static IEstimator<ITransformer> ProccesData()
-        {
-            var pipeline = _mlContext.Transforms.Conversion.MapValueToKey(inputColumnName: "Area", outputColumnName: "Label")
-                .Append(_mlContext.Transforms.Text.FeaturizeText("TitleFeaturized", "Title"))
-                .Append(_mlContext.Transforms.Text.FeaturizeText(inputColumnName: "Description", outputColumnName: "DescriptionFeaturized"))
-                .Append(_mlContext.Transforms.Concatenate("Features", "TitleFeaturized", "DescriptionFeaturized"))
-                .AppendCacheCheckpoint(_mlContext);//for cache using (only for small or midlle datasets)
-
-            return pipeline;
-        }
-
-        private static IEstimator<ITransformer> BuildAndTrainModel(IDataView TrainDataset,IEstimator<ITransformer> pipeline)
-        {
-            Console.WriteLine("Build and Training");
-            var trainingPipeline = pipeline.Append(_mlContext.MulticlassClassification.Trainers.SdcaMaximumEntropy("Label", "Features"))
-                .Append(_mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
-
-            _trainedModel = trainingPipeline.Fit(TrainDataset);
-            _predEngine = _mlContext.Model.CreatePredictionEngine<IssueModel, IssuePrediction>(_trainedModel);
-
-            IssueModel issue = new IssueModel()
+            Console.WriteLine("Operations \n1) deffault start \n2)testing");
+            int variable = 0;
+            while (variable == 0)
             {
-                Title = "WebSockets communication is slow in my machine",
-                Description = "The WebSockets communication used under the covers by SignalR looks like is going slow in my development machine.."
-            };
+                int.TryParse(Console.ReadLine(),out variable);
+            }
+            switch (variable)
+            {
+                case 1:
+                    ML.Ml_Steps._trainingDataView = ML.Ml_Steps._mlContext.Data.LoadFromTextFile<IssueModel>(ML.Ml_Steps._trainDataPath, hasHeader: true);
 
-            var prediction = _predEngine.Predict(issue);
+                    var pipeline = ML.Ml_Steps.ProccesData();
 
-            Console.WriteLine($"=============== Single Prediction just-trained-model - Result: {prediction.Area} ===============");
-            return trainingPipeline;
+                    var trainingPipelane = ML.Ml_Steps.BuildAndTrainModel(ML.Ml_Steps._trainingDataView, pipeline);
+
+                    ML.Ml_Steps.Evaluate(ML.Ml_Steps._trainingDataView.Schema);
+
+                    ML.Ml_Steps.SaveModelAsFile(ML.Ml_Steps._mlContext, ML.Ml_Steps._trainingDataView.Schema, ML.Ml_Steps._trainedModel);
+
+                    //ML.Ml_Steps.PredictIssue();
+                    break;
+
+                case 2:
+                    IssueModel model = new IssueModel();
+                    Console.WriteLine("Test model Title");
+                    model.Title = Console.ReadLine();
+                    Console.WriteLine("Test model Description");
+                    model.Description = Console.ReadLine();
+                    ML.Ml_Steps.Ml_Testing(model);
+                   break;
+            }
+           
+
         }
 
-        private static void Evaluate(DataViewSchema trainingDataViewSchema)
-        {
-            var testDataView = _mlContext.Data.LoadFromTextFile<IssueModel>(_testDataPath, hasHeader: true);
-
-            var testMetrics = _mlContext.MulticlassClassification.Evaluate(_trainedModel.Transform(testDataView));
-
-
-            Console.WriteLine($"*************************************************************************************************************");
-            Console.WriteLine($"*       Metrics for Multi-class Classification model - Test Data     ");
-            Console.WriteLine($"*------------------------------------------------------------------------------------------------------------");
-            Console.WriteLine($"*       MicroAccuracy:    {testMetrics.MicroAccuracy:0.###}");
-            Console.WriteLine($"*       MacroAccuracy:    {testMetrics.MacroAccuracy:0.###}");
-            Console.WriteLine($"*       LogLoss:          {testMetrics.LogLoss:#.###}");
-            Console.WriteLine($"*       LogLossReduction: {testMetrics.LogLossReduction:#.###}");
-            Console.WriteLine($"*************************************************************************************************************");
-        }
-
-        private static void SaveModelAsFile(MLContext mLContext,DataViewSchema trainingDataSchema,ITransformer model)
-        {
-            mLContext.Model.Save(model, trainingDataSchema, _modelPath);
-        }
+      
     }
 }
